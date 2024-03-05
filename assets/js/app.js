558be66bbc1299bb756dcd3cc1ff4676bdc62dd6
Seb-mlrd <sebmlrd06@gmail.com>
2024-03-05 21:41:17 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+"use strict"
+
+let slides = document.querySelectorAll('.imgCarroussel');
+let index = 0;
+
+function changeSlide() {
+    if(index < 0){
+        index = slides.length - 1;
+    }else if(index>=slides.length){
+        index=0;
+    }
+
+    slides.forEach( slide =>{
+        slide.classList.remove('active');
+    })
+
+    slides[index].classList.add('active');
+    index++;
+
+}
+setInterval(changeSlide, 2000)
